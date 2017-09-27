756793e7e2d772a37d2d5751a11fe9a1691ab13f
emir <cengice@hotmail.com>
2017-09-27 18:06:41 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,87 @@
-import { Component } from '@angular/core';
+import {NgModule, Component, Injectable} from '@angular/core';
+//import { Http , Response} from '@angular/http' ;
+import {JsonpModule, Jsonp, Response} from '@angular/http';
+import {Observable} from 'rxjs';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/do';
+
+class SearchItem {
+  constructor(public track: string,
+              public artist: string,
+              public link: string,
+              public thumbnail: string,
+              public artistId: string) {
+  }
+}
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+@Injectable()
 export class AppComponent {
-  title = 'app';
+	apiRoot: string = 'https://itunes.apple.com/search?term=good';
+	//https://itunes.apple.com/search?term=love&media=music&limit=3/
+	term: string = 'good'; 
+	//sufix: string= '&media=music&limit=2&callback=JSONP_CALLBACK`;
+//	apiAll = apiRoot + term ;//+ sufix;
+	
+/* json */
+	constructor(private jsonp: Jsonp) {
+	}
+
+ searchCity(term: string) {
+	console.log("Done in");
+	
+//	console.log(`${this.apiRoot}?term=${term}&media=music&limit=2&callback=JSONP_CALLBACK`);
+	console.log(`${this.apiRoot}&media=music&limit=2&callback=JSONP_CALLBACK`);
+	
+//   let apiURL = `${this.apiRoot}?term=${term}&media=music&limit=2&callback=JSONP_CALLBACK`;
+   let apiURL = `${this.apiRoot}&media=music&limit=2&callback=JSONP_CALLBACK`;
+    return this.jsonp.request(apiURL)
+        .map(res => { return res.json().results.map(item => {
+			console.log("Done out");
+			  
+            return new SearchItem(
+                item.trackName,
+                item.artistName,
+                item.trackViewUrl,
+                item.artworkUrl30,
+                item.artistId
+            );
+          });
+        });
+  }
 }
+
+
+
+
+/*	
+	constructor(private http: Http) { }
+	cityName = '';
+	cityHumid = '';
+	
+searchCity() {
+
+this.http.get('http://api.openweathermap.org/data/2.5/weather?APPID=0ba5e82df49a15a79cec569618c56215&q=' + this.cityName )
+.subscribe (
+(res: Response) => {const weatherCity = res.json() ;
+console.log(weatherCity)
+this.cityHumid = weatherCity.main.humidity;
+this.cityName = weatherCity.main.city;
+console.log(this.cityName);
+}
+)
+
+}
+	title = 'app works';
+	
+}
+*/
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,14 @@
+import {NgModule, Component, Injectable} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http' ;
+import {JsonpModule, Jsonp, Response} from '@angular/http';
+import {Observable} from 'rxjs';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/do';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +17,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+	FormsModule,
+	HttpModule,
+	JsonpModule
+	
   ],
   providers: [],
   bootstrap: [AppComponent]
